2dbfc9411cae5e15d65c959c6c5497399068f17a
Denis Freund <info@denis-freund.de>
2022-05-02 13:32:18 +0200

    add msfs package

---
+++ scripts/package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+# copy from build into PackageSource
+cp -rva ./headwind-aircraft-a330-900/. ./src-a339/package/PackageSources
+
+cd /src-a339/package/PackageSources
+find . -type f -iname \*.PNG.DDS -delete
+find . -type f -iname \*.PNG.DDS.json -delete
