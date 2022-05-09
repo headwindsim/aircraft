32a860fb1a506a0312beeb65b8fa2fe193830746
Denis Freund <info@denis-freund.de>
2022-05-09 13:12:01 +0200

    change sentry prefix

---
+++ src-a339/instruments/src/Common/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Defaults from './defaults';
+import { SimVarProvider } from './simVars';
+import { FbwAircraftSentryClient } from '../../../sentry-client/src/FbwAircraftSentryClient';
+
+declare const process: any;
+
+/**
+ * Use the given React element to render the instrument using React.
+ */
+export const render = (Slot: React.ReactElement, enableSentryTracing = false, sentryRootClient = false) => {
+    new FbwAircraftSentryClient().onInstrumentLoaded({
+        dsn: process.env.SENTRY_DSN,
+        buildInfoFilePrefix: 'a339x',
+        enableTracing: enableSentryTracing,
+        root: sentryRootClient,
+    });
+
+    ReactDOM.render(<SimVarProvider>{Slot}</SimVarProvider>, Defaults.getRenderTarget());
+};
+
+/**
+ * Computes time delta out of absolute env time and previous
+ * time debounced on time shift.
+ */
+export const debouncedTimeDelta = (
+    absTimeSeconds: number,
+    prevTimeSeconds: number,
+): number => {
+    const diff = Math.max(absTimeSeconds - prevTimeSeconds, 0);
+    // 60s detects forward time-shift
+    return diff < 60 ? diff : 0;
+};
---
+++ src-a339/sentry-client/src/index.ts
@@ -0,0 +1,10 @@
+import { FbwAircraftSentryClient } from './FbwAircraftSentryClient';
+
+declare const process: any;
+
+new FbwAircraftSentryClient().onInstrumentLoaded({
+    dsn: process.env.SENTRY_DSN,
+    buildInfoFilePrefix: 'a339x',
+    root: false,
+    enableTracing: false,
+});
