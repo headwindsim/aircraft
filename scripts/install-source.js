527762035b72bb827ba501b6bcc5438a5b09b530
Denis Freund <info@denis-freund.de>
2022-05-12 23:18:59 +0200

    update to rc4, fix sound.xml simvar value

--- scripts/install-source.js
+++ scripts/install-source.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const fs = require('fs-extra');
 
-const source = process.env['BUILD_DIR_NAME'] ? 'external/a339x/' + process.env['BUILD_DIR_NAME'] : 'external/a339x';
+const source = process.env.BUILD_DIR_NAME ? 'external/a339x/' + process.env.BUILD_DIR_NAME : 'external/a339x';
 console.log('installManifest source is: ' + source);
 
 const installManifest = fs.readJSONSync('./headwind-aircraft-a330-900/install.json');
