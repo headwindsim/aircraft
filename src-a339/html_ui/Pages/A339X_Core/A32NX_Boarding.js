217fbe3d7b1ab587f22c399f00a534e97253565d
Denis Freund <info@denis-freund.de>
2022-10-15 16:34:40 +0200

    change to 436 pax

--- src-a339/html_ui/Pages/A339X_Core/A32NX_Boarding.js
+++ src-a339/html_ui/Pages/A339X_Core/A32NX_Boarding.js
@@ -59,9 +59,9 @@ class A32NX_Boarding {
             paxRemaining -= pax;
         }
 
-        await fillStation(paxStations['rows26_42'], .4311 , numberOfPax);
-        await fillStation(paxStations['rows12_25'], .3863, numberOfPax);
-        await fillStation(paxStations['rows9_11'], .0725, numberOfPax);
+        await fillStation(paxStations['rows26_42'], .326 , numberOfPax);
+        await fillStation(paxStations['rows12_25'], .207, numberOfPax);
+        await fillStation(paxStations['rows9_11'], .214, numberOfPax);
         await fillStation(paxStations['rows1_8'], 1 , paxRemaining);
         return;
     }
