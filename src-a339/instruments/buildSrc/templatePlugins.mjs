a2d07ac103c04098941e28fcc7377e86b064fa12
Denis Freund <info@denis-freund.de>
2023-01-11 00:44:33 +0100

    refactor: port SD to full react-based instrument (#7345)
    fix panel

--- src-a339/instruments/buildSrc/templatePlugins.mjs
+++ src-a339/instruments/buildSrc/templatePlugins.mjs
@@ -1,20 +1,13 @@
 import { join } from 'path';
 import instrumentTemplate from '@flybywiresim/rollup-plugin-msfs';
-import ecamPageTemplate from '../ecam-page-template/rollup.js';
 import { Directories } from './directories.mjs';
 
-export function getTemplatePlugin({ name, config, imports = [], isInstrument }) {
-    if (isInstrument) {
-        return instrumentTemplate({
-            name,
-            elementName: `a32nx-${name.toLowerCase()}`,
-            config,
-            imports,
-            outputDir: join(Directories.root, 'headwind-aircraft-a330-900/html_ui/Pages/VCockpit/Instruments/A339X'),
-        });
-    }
-    return ecamPageTemplate({
+export function getTemplatePlugin({ name, config, imports = [] }) {
+    return instrumentTemplate({
         name,
-        outputDir: join(Directories.root, 'headwind-aircraft-a330-900/html_ui/Pages/VCockpit/Instruments/A339X/EcamPages'),
+        elementName: `a32nx-${name.toLowerCase()}`,
+        config,
+        imports,
+        outputDir: join(Directories.root, 'headwind-aircraft-a330-900/html_ui/Pages/VCockpit/Instruments/A339X'),
     });
 }
