4376b36bf0fb1b2020e386419ca7fa79c2a810ce
Denis Freund <info@denis-freund.de>
2023-01-27 16:12:40 +0100

    fix: adjust position wheel page

--- src-a339/instruments/src/SD/Pages/Wheel/Wheel.tsx
+++ src-a339/instruments/src/SD/Pages/Wheel/Wheel.tsx
@@ -497,8 +497,8 @@ const Wheels = ({ x, y, left, right }: WheelsProps) => {
             <WheelArch x={0} y={103} type="bottom" />
             <WheelArch x={124} y={103} type="bottom" />
 
-            <text className="Cyan Standard" x={73} y={32}>°C</text>
-            <text className="Standard" x={74} y={66}>R</text>
+            <text className="Cyan Standard" x={73} y={40}>°C</text>
+            <text className="Standard" x={87} y={66}>R</text>
 
             <text className={`${left.temperature > brakeAmberThreshold ? 'Amber' : 'Green'} Large End`} x={57} y={33}>{Math.max(0, Math.round(left.temperature / 5) * 5)}</text>
             <text className={`${right.temperature > brakeAmberThreshold ? 'Amber' : 'Green'} Large End`} x={181} y={33}>{Math.max(0, Math.round(right.temperature / 5) * 5)}</text>
