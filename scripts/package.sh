5ddf28e447b083f06dd089a8f24d446500d627d5
Kevin Widjaja <kevin.widjaja21@gmail.com>
2023-02-09 22:16:32 +0800

    Change package script to new paths after refactor

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,8 +3,8 @@
 set -ex
 
 # copy from build into PackageSource
-cp -rva ./headwind-aircraft-a330-900/. ./src-a339/project/PackageSources
+cp -rva ./hdw-a339x/out/headwindsim-aircraft-a330-900/. ./hdw-a339x/src/project/PackageSources
 
-cd /external/src-a339/project/PackageSources
+cd /external/hdw-a339x/src/project/PackageSources
 find . -type f -iname \*.PNG.DDS -delete
 find . -type f -iname \*.PNG.DDS.json -delete
