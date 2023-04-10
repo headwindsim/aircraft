83f49fbdc39f74eb373898dc4f4232dcd534f90f
Denis Freund <info@denis-freund.de>
2023-04-11 01:50:16 +0200

    update(light): rework and update of light system

--- scripts/copy_effect.sh
+++ scripts/copy_effect.sh
@@ -3,18 +3,18 @@
 set -ex
 
 #copy and rename effect file when it is changed. Still need to change the texture file name inside the fx file manually
-rm -rvf ./headwind-aircraft-a330-900/effects
+rm -rvf ./hdw-a339x/src/base/headwindsim-aircraft-a330-900/effects
 
-cp -rva ./a32nx/flybywire-aircraft-a320-neo/effects/. ./headwind-aircraft-a330-900/effects
+cp -rva ./a32nx/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/effects/. ./hdw-a339x/src/base/headwindsim-aircraft-a330-900/effects
 
-for nam in ./headwind-aircraft-a330-900/effects/*A32NX*.fx
+for nam in ./hdw-a339x/src/base/headwindsim-aircraft-a330-900/effects/*A32NX*.fx
 do
-    newname=${nam/A32NX/A339}
+    newname=${nam/A32NX/A339X}
     mv $nam $newname
 done
 
-for nam in ./headwind-aircraft-a330-900/effects/texture/*A32NX*
+for nam in ./hdw-a339x/src/base/headwindsim-aircraft-a330-900/effects/texture/*A32NX*
 do
-    newname=${nam/A32NX/A339}
+    newname=${nam/A32NX/A339X}
     mv $nam $newname
 done
