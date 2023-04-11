ebf042f46b1c27ea285ac1430ae570ec3d51567a
Denis Freund <info@denis-freund.de>
2023-04-11 03:30:17 +0200

    chore: upgrade msfssdk to 0.4.0, remove fork
    FBW PR 7909
    fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,17 +7,6 @@ git config --global --add safe.directory "*"
 # initialize submodule (a32nx)
 git submodule update --init --recursive
 
-cd a32nx/fbw-common/msfs-avionics-mirror/src/msfstypes
-npm pack
-cd ../sdk
-rm -rf node_modules
-rm -rf build
-npm ci
-npm run build
-cp package.json build/
-cd build
-npm pack
-
 cd /external
 rm -rf node_modules
 npm ci
