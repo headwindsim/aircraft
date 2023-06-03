ae25250beea50348bccc82b977f449d25fd32b62
Denis Freund <info@denis-freund.de>
2023-06-03 11:38:39 +0200

    chore: further cleanup for github action

--- scripts/build_a339x.sh
+++ scripts/build_a339x.sh
@@ -18,6 +18,8 @@ if [ "${GITHUB_ACTIONS}" == "true" ]; then
   rm -rf /external/flybywire
   rm -rf /external/build-a339x/src
   rm -rf /external/build-acj339x/src
+  rm -rf /external/hdw-a339x/src
+  rm -rf /external/hdw-acj339x/src
 fi
 
 # restore ownership (when run as github action)
