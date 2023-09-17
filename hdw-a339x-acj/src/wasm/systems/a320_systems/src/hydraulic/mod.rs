7641daed537ad6204a57e0cd5732d192a5b78352
Robin Breitfeld <robin01196@gmail.com>
2023-09-17 18:28:48 +0200

    fix(wasm): fix acj hydraulic mod file (#73)
    * feat(eng): add RR Trent-900 based FADEC
    
    * feat(eng): use Trent engine model instead of LEAP
    
    * update engine thrust
    
    * minor fixes on build process
    
    * fix(ewd): read N3 instead of N2
    
    * fix(ewd): add variable to monitor N3 SimVar
    
    * fix(system): acj hydraulic file

--- hdw-a339x-acj/src/wasm/systems/a320_systems/src/hydraulic/mod.rs
+++ hdw-a339x-acj/src/wasm/systems/a320_systems/src/hydraulic/mod.rs
@@ -6040,7 +6040,7 @@ impl SimulationElement for A330TiltingGears {
 mod tests {
     use super::*;
 
-    mod a320_hydraulics {TrentEngine
+    mod a320_hydraulics {
         use super::*;
         use systems::{
             electrical::{
