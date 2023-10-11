590518f681fe2d24cb06df6b8c02a8398f4b4e1f
Revyn112 <info@denis-freund.de>
2023-10-11 12:26:05 +0200

    chore: update workflows
    fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,9 +4,16 @@ set -ex
 
 git config --global --add safe.directory "*"
 
-# initialize submodule (a32nx)
+# initialize submodule
 git submodule update --init --recursive
 
 cd /external
-rm -rf node_modules
+
+for arg in "$@"; do
+  if [ "$arg" = "--clean" ]; then
+    echo "Removing node_modules..."
+    rm -rf node_modules/
+  fi
+done
+
 npm ci
