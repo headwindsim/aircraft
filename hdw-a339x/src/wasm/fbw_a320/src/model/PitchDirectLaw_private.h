f22de7924dd05a9d7d84768d7112b303125a0dc2
kevinwidjaja21 <kevin.widjaja21@gmail.com>
2023-11-11 23:32:37 +0800

    Remove unnecessary changes

--- hdw-a339x/src/wasm/fbw_a320/src/model/PitchDirectLaw_private.h
+++
@@ -1,6 +0,0 @@
-#ifndef RTW_HEADER_PitchDirectLaw_private_h_
-#define RTW_HEADER_PitchDirectLaw_private_h_
-#include "rtwtypes.h"
-#include "PitchDirectLaw_types.h"
-#endif
-
--- hdw-a339x/src/wasm/fbw_a320/src/model/SecComputer_private.h
+++
@@ -1,6 +0,0 @@
-#ifndef RTW_HEADER_SecComputer_private_h_
-#define RTW_HEADER_SecComputer_private_h_
-#include "rtwtypes.h"
-#include "SecComputer_types.h"
-#endif
-
