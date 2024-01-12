76d8e40e40dddc57818c577dee54c1f58623cb90
Denis Freund <info@denis-freund.de>
2024-01-12 09:07:32 +0100

    fix(flight_model): adjust wing area in vnav params and flight_model
    fix(flight_model): adjust wing area in vnav params and flight_model

--- hdw-a339x/src/systems/fmgc/src/guidance/vnav/FlightModel.ts
+++ hdw-a339x/src/systems/fmgc/src/guidance/vnav/FlightModel.ts
@@ -2,11 +2,11 @@ import { MathUtils } from '@flybywiresim/fbw-sdk';
 import { Common, FlapConf } from './common';
 
 export class FlightModel {
-    static Cd0 = 0.0200;
+    static Cd0 = 0.0260;
 
     static wingSpan = 209.97;
 
-    static wingArea = 5010;
+    static wingArea = 4000;
 
     static wingEffcyFactor = 0.72;
 
