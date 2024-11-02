c16a705abaa488ce038093d4a5aea8d191b1ae5d
Denis Freund <info@denis-freund.de>
2024-11-02 15:43:02 +0100

    chore: use new jest style config
    FBW PR 8689

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,18 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./build-common/src/jest/setupJestMock.js'],
-  globals: {
-    'ts-jest': {
-      // Babel assumes isolated modules, therefore enable it here as well.
-      // This also speeds up the unit testing performance.
-      isolatedModules: true,
-      diagnostics: {
-        ignoreCodes: ['TS151001'],
+  transform: {
+    '.spec.ts$': [
+      'ts-jest',
+      {
+        // Babel assumes isolated modules, therefore enable it here as well.
+        // This also speeds up the unit testing performance.
+        isolatedModules: true,
+        diagnostics: {
+          ignoreCodes: ['TS151001'],
+        },
       },
-    },
+    ],
   },
   modulePathIgnorePatterns: [
     'hdw-a333x/src/systems/fmgc/src/flightplanning',
