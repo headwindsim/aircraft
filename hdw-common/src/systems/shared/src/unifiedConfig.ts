2e6e6bc33d3a447d437572346c9155042de8b4a3
Denis Freund <info@denis-freund.de>
2024-11-07 00:02:44 +0100

    fix: 2-axes throttle calibration
    FBW PR 8953

--- hdw-common/src/systems/shared/src/unifiedConfig.ts
+++ hdw-common/src/systems/shared/src/unifiedConfig.ts
@@ -59,7 +59,7 @@ export interface FlypadInfo {
 }
 
 export interface ThrottleFlypadInfo {
-  numberOfAxis: number;
+  numberOfAircraftThrottles: number;
   axisOptions: number[];
   axisMapping: ThrottleFlypadAxisMapping;
 }
