c0ed205bae393d323e2236e459ab9935c72a1985
Denis Freund <info@denis-freund.de>
2024-11-07 03:31:33 +0100

    feat(payload): redone boarding agents (priority boarding, stairs, etc)
    FBW PR 8985

--- hdw-a333x/src/wasm/systems/a320_systems/src/payload/mod.rs
+++ hdw-a333x/src/wasm/systems/a320_systems/src/payload/mod.rs
@@ -196,18 +196,21 @@ impl A320Payload {
                 Mass::new::<kilogram>(c.max_cargo_kg),
             )
         });
+
+        let default_boarding_agent = BoardingAgent::new(None, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
         let boarding_agents = [
             BoardingAgent::new(
-                context.get_identifier("INTERACTIVE POINT OPEN:0".to_owned()),
+                Some(context.get_identifier("INTERACTIVE POINT OPEN:0".to_owned())),
                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             ),
             BoardingAgent::new(
-                context.get_identifier("INTERACTIVE POINT OPEN:2".to_owned()),
+                Some(context.get_identifier("INTERACTIVE POINT OPEN:2".to_owned())),
                 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
             ),
         ];
 
-        let passenger_deck = PassengerDeck::new(pax, boarding_agents);
+        let passenger_deck = PassengerDeck::new(pax, default_boarding_agent, boarding_agents);
         let cargo_deck = CargoDeck::new(cargo);
 
         A320Payload {
