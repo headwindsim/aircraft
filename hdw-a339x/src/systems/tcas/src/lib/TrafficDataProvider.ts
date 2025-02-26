c1ade503e2f2a9f67459a4b7fed41553516e601e
Liz3 <accs@liz3.net>
2025-02-26 21:46:24 +0100

    aaaaaa

--- hdw-a339x/src/systems/tcas/src/lib/TrafficDataProvider.ts
+++ hdw-a339x/src/systems/tcas/src/lib/TrafficDataProvider.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export function getData(id, port, network) {
-  const network = [null, null, 'vatsim', 'ivao'];
-  return axios.get(`http://localhost:${port.toFixed(0)}/data/${id}${network ? `?n=${network}` : ''}`);
+  const nstr = [null, null, 'vatsim', 'ivao'][network];
+  return axios.get(`http://localhost:${port.toFixed(0)}/data/${id}${nstr ? `?n=${nstr}` : ''}`);
 }
