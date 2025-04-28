e46f3622d064455a8ebbec5777256dc962bd29c7
Denis Freund <info@denis-freund.de>
2025-04-28 15:40:41 +0200

    fix: stuck popup (again)
    FBW PR 10012

--- hdw-common/src/systems/shared/src/FbwAircraftSentryClient.ts
+++ hdw-common/src/systems/shared/src/FbwAircraftSentryClient.ts
@@ -10,7 +10,6 @@ import { CaptureConsole as CaptureConsoleIntegration } from '@sentry/integration
 import { Integration } from '@sentry/types';
 import { NXDataStore } from './persistence';
 import { PopUpDialog } from './popup';
-import { SimVarValueType, Wait } from '@microsoft/msfs-sdk';
 
 export const SENTRY_CONSENT_KEY = 'SENTRY_CONSENT';
 
@@ -114,31 +113,25 @@ export class FbwAircraftSentryClient {
           const instrument = document.querySelector('vcockpit-panel > *');
 
           if (instrument) {
-            Wait.awaitCondition(
-              () => SimVar.GetSimVarValue('L:FBW_IN_FLIGHT_DECK', SimVarValueType.Bool) === 1,
-              60,
-              60000,
-            ).then(() => {
-              resolve(
-                FbwAircraftSentryClient.requestConsent()
-                  .then((didConsent) => {
-                    if (didConsent) {
-                      NXDataStore.set(SENTRY_CONSENT_KEY, SentryConsentState.Given);
+            resolve(
+              FbwAircraftSentryClient.requestConsent()
+                .then((didConsent) => {
+                  if (didConsent) {
+                    NXDataStore.set(SENTRY_CONSENT_KEY, SentryConsentState.Given);
 
-                      console.log('[SentryClient] User requested consent state Given. Initializing sentry');
+                    console.log('[SentryClient] User requested consent state Given. Initializing sentry');
 
-                      return FbwAircraftSentryClient.attemptInitializeSentry(config);
-                    }
+                    return FbwAircraftSentryClient.attemptInitializeSentry(config);
+                  }
 
-                    NXDataStore.set(SENTRY_CONSENT_KEY, SentryConsentState.Refused);
+                  NXDataStore.set(SENTRY_CONSENT_KEY, SentryConsentState.Refused);
 
-                    console.log('[SentryClient] User requested consent state Refused. Doing nothing');
+                  console.log('[SentryClient] User requested consent state Refused. Doing nothing');
 
-                    return false;
-                  })
-                  .catch(() => false),
-              );
-            });
+                  return false;
+                })
+                .catch(() => false),
+            );
           } else {
             reject(new Error('[SentryClient] Could not find an instrument element to hook onto'));
           }
@@ -165,7 +158,7 @@ export class FbwAircraftSentryClient {
     return new Promise<boolean>((resolve) => {
       popup.showPopUp(
         'HEADWIND - ERROR REPORTING',
-        'Are you willing to help FlyByWire Simulations by enabling anonymous reporting of errors that may occur in the future? ' +
+        'Are you willing to help Headwind Simulations by enabling anonymous reporting of errors that may occur in the future? ' +
           'This is 100% optional and we will never collect your personal data, but it will help us diagnose issues quickly.',
         'normal',
         () => resolve(true),
