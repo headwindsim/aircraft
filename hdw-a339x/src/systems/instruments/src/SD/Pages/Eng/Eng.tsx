f65331ed3d41c114ad1e63ef77a9da680dc77a5d
Jonny23787 <Jonny.leske@outlook.com>
2025-05-12 18:28:43 +0930

    fix(a339x/sd): remove amber oil qt indications (#126)

--- hdw-a339x/src/systems/instruments/src/SD/Pages/Eng/Eng.tsx
+++ hdw-a339x/src/systems/instruments/src/SD/Pages/Eng/Eng.tsx
@@ -243,24 +243,6 @@ const QuantityGauge = ({ x, y, engineNumber, fadecOn }: ComponentPositionProps) 
           className={`NoFill ${displayedEngineOilQuantity === 0 && 'Hidden'} ${shouldQuantityPulse ? 'LinePulse' : 'GreenLine '}`}
           dashOffset={-40}
         />
-        <Needle
-          x={x}
-          y={y}
-          length={60}
-          scaleMax={100}
-          value={getNeedleValue(OIL_QTY_LOW_ADVISORY, OIL_QTY_MAX) - 3}
-          className="NoFill AmberHeavy"
-          dashOffset={-50}
-        />
-        <Needle
-          x={x}
-          y={y}
-          length={50}
-          scaleMax={100}
-          value={getNeedleValue(OIL_QTY_LOW_ADVISORY, OIL_QTY_MAX) - 2}
-          className="NoFill AmberLine"
-          dashOffset={-45}
-        />
         <text x={x + 5} y={y} className={`FontLarge TextCenter ${shouldQuantityPulse ? 'FillPulse' : 'FillGreen'}`}>
           <tspan className="FontLarge">{displayedEngineOilQuantity.toFixed(1).split('.')[0]}</tspan>
           <tspan className="FontSmall">.</tspan>
