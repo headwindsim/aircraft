e54674817c3c0b74ba64ce6a63d2d1bd36c0995e
Jonny23787 <Jonny.leske@outlook.com>
2025-06-15 20:50:03 +0930

    change from nothing to false

--- hdw-a339x/src/systems/instruments/src/EFB/index.tsx
+++ hdw-a339x/src/systems/instruments/src/EFB/index.tsx
@@ -37,6 +37,7 @@ render(
           engineVolume: true,
           masterVolume: true,
           windVolume: true,
+          ptuCockpit: false,
           paxAmbience: true,
         },
         // FIXME: just inject the aircraft options page from the aircraft context (or plugin in flypadOSv4).
